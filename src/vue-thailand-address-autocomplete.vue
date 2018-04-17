2e611a862574394f6fe4c9475a87502b9813cddb
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-04-17 12:47:02 +0700

    add thailand address lib

--- src/vue-thailand-address-autocomplete.vue
+++ src/vue-thailand-address-autocomplete.vue
@@ -3,8 +3,21 @@
     <input type="text" :value="type">
   </div>
 </template>
+<template>
+  <div id="app">
+    ตำบล
+    <input v-model="q">
+    <ul>
+      <li v-for="item in result">
+        {{item.district}} » {{item.amphoe}} » {{item.province}} » {{item.zipcode}}
+      </li>
+    </ul>
+  </div>
+</template>
 
 <script>
+import { searchAddressByDistrict } from 'thai-address-database'
+
 export default {
   name: 'vue-thailand-address-autocomplete',
   props: {
@@ -16,16 +29,13 @@ export default {
   },
   data () {
     return {
+      q: ''
     }
   },
-  mounted () {
-  },
-  watch: {
-    type (val) {
-      console.log(val)
+  computed: {
+    result () {
+      return searchAddressByDistrict(this.q)
     }
-  },
-  methods: {
   }
 }
 </script>
